c3f6ca8ccfc4b785a6a2dbeabf98d872fbaa229f
David Kočnar <david.kocnar@futured.app>
2023-06-01 16:28:39 +0200

    Add internet permission and base url to api

--- app/src/main/kotlin/app/futured/academyproject/tools/Constants.kt
+++ app/src/main/kotlin/app/futured/academyproject/tools/Constants.kt
@@ -3,7 +3,7 @@ package app.futured.academyproject.tools
 interface Constants {
 
     object Api {
-        const val BASE_PROD_URL = "https://reqres.in/"
+        const val BASE_PROD_URL = "https://gis.brno.cz/ags1/rest/services/OMI/"
         const val TIMEOUT_IN_SECONDS = 30L
     }
 }

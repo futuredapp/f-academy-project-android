c5e2774789a030656e0d5da983b5ec433a40be57
David Kočnar <david.kocnar@futured.app>
2023-06-06 16:50:30 +0200

    Fix detekt lints

--- app/src/main/kotlin/app/futured/academyproject/tools/Constants.kt
+++ app/src/main/kotlin/app/futured/academyproject/tools/Constants.kt
@@ -6,4 +6,9 @@ interface Constants {
         const val BASE_PROD_URL = "https://gis.brno.cz/ags1/rest/services/OMI/"
         const val TIMEOUT_IN_SECONDS = 30L
     }
+
+    object Ui {
+        const val PLACE_CARD_ASPECT_RATIO = 1.7f
+        const val GRADIENT_OVERLAY_ALPHA = 0.7f
+    }
 }

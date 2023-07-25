f24ad5b9ed75f8ed18f65cac96b1f0f8806e57eb
jan.mikulik <jan.mikulik@futured.app>
2023-07-25 13:59:28 +0200

    Add Task 3

---
+++ app/src/test/kotlin/app/futured/academyproject/Task3Test.kt
@@ -0,0 +1,15 @@
+package app.futured.academyproject
+
+import org.junit.Test
+
+class Task3Test {
+
+    @Test
+    fun santaAndGoblins() {
+        /**
+         * Santa ma na starosti 10 skritku
+         * Kazdy skritek vykonava nejakou praci od 100 ms po 1 s
+         * Santa by mel pockat az skritci dokonci svoji praci a potom teprve muze ukoncit svoji
+         */
+    }
+}
